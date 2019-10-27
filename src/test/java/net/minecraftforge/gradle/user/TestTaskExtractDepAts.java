439df08b1bc68c9f9bd5a88ef14c60f898139a42
CDAGaming <cstack2011@yahoo.com>
2019-10-26 23:28:52 -0500

    [Change] Gradle 5.x Compatibility
    Allows Building on Gradle 4.10 and upwards through 5.x

--- src/test/java/net/minecraftforge/gradle/user/TestTaskExtractDepAts.java
+++ src/test/java/net/minecraftforge/gradle/user/TestTaskExtractDepAts.java
@@ -21,8 +21,10 @@ package net.minecraftforge.gradle.user;
 
 import net.minecraftforge.gradle.testsupport.TaskTest;
 import net.minecraftforge.gradle.testsupport.TestResource;
+import org.gradle.api.Project;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.internal.artifacts.dependencies.DefaultSelfResolvingDependency;
-import org.gradle.api.internal.file.collections.SimpleFileCollection;
+import org.gradle.api.internal.file.FileCollectionInternal;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,7 +41,7 @@ public class TestTaskExtractDepAts extends TaskTest<TaskExtractDepAts>
             throw new IOException("Couldn't create file in " + outputDir);
         File modWithAtJar = TestResource.MOD_WITH_AT.getFile(temporaryFolder);
         TaskExtractDepAts task = getTask(TaskExtractDepAts.class);
-        task.getProject().getConfigurations().maybeCreate("test_at").getDependencies().add(new DefaultSelfResolvingDependency(new SimpleFileCollection(modWithAtJar)));
+        task.getProject().getConfigurations().maybeCreate("test_at").getDependencies().add(new DefaultSelfResolvingDependency((FileCollectionInternal) task.getProject().files(modWithAtJar)));
         task.addCollection("test_at");
         task.setOutputDir(outputDir);
         task.doTask();
