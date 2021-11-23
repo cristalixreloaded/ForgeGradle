5144de6021630d69c93c6b529d2df14ab19f44a5
Egor Koleda <radioegor146@yandex.ru>
2021-11-23 07:12:59 +0300

    fix mcp for FG 1.2

--- src/main/java/net/minecraftforge/gradle/common/Constants.java
+++ src/main/java/net/minecraftforge/gradle/common/Constants.java
@@ -48,15 +48,15 @@ public class Constants {
     public static final String MC_JSON_URL = "http://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.json";
     public static final String MC_JAR_URL = "http://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.jar";
     public static final String MC_SERVER_URL = "http://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/minecraft_server.{MC_VERSION}.jar";
-    public static final String MCP_URL = "http://files.minecraftforge.net/fernflower-fix-1.0.zip";
+    public static final String MCP_URL = "https://files.minecraftforge.net/fernflower-fix-1.0.zip";
     public static final String ASSETS_URL = "http://resources.download.minecraft.net";
     public static final String LIBRARY_URL = "https://libraries.minecraft.net/";
-    public static final String FORGE_MAVEN = "http://files.minecraftforge.net/maven";
+    public static final String FORGE_MAVEN = "https://maven.minecraftforge.net";
     public static final String ASSETS_INDEX_URL = "https://s3.amazonaws.com/Minecraft.Download/indexes/{ASSET_INDEX}.json";
 
     // MCP things
     public static final String CONFIG_MCP_DATA = "mcpSnapshotDataConfig";
-    public static final String MCP_JSON_URL = "http://export.mcpbot.bspk.rs/versions.json";
+    public static final String MCP_JSON_URL = FORGE_MAVEN + "/de/oceanlabs/mcp/versions.json";
 
     // things in the cache dir.
     public static final String NATIVES_DIR = "{CACHE_DIR}/minecraft/net/minecraft/minecraft_natives/{MC_VERSION}";
